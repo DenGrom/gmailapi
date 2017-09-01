92c295787a4e4e2cd8bd999cdf9cc201c1998ada
DenGrom <156789QQ@gmail.com>
2017-09-01 11:29:34 +0300

    classes user

---
+++ GmailApi/Scripts/App/models/userModel.js
@@ -0,0 +1,5 @@
+﻿function User(firstName, lastName, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+}
--- GmailApi/Scripts/App/сontrollers/userController.js
+++ GmailApi/Scripts/App/сontrollers/userController.js
@@ -1,13 +1,14 @@
 ﻿app.controller('userController', ['$http', '$scope', 'userService', function ($http, $scope, userService) {
 
-
+    //$scope.user = "User";
     $scope.addUser = function () {
-        
+        var user = new User($scope.firstName, $scope.lastName, $scope.email);
+        console.log(user);
     }
 
-    $scope.user.firstName = "Alex";
-    $scope.user.lastName = "Bordov";
-    $scope.user.email = "alexbordov1@gmail.com";
-    $scope.user.imageUrl = "https://lh4.googleusercontent.com/-5Y__F7y9tT8/AAAAAAAAAAI/AAAAAAAAAAA/APJypA0c8GceAD1En_08aa9B9Db6-nzfvg/s96-c/photo.jpg";
+    //$scope.user.firstName = "Alex";
+    //$scope.user.lastName = "Bordov";
+    //$scope.user.email = "alexbordov1@gmail.com";
+    //$scope.user.imageUrl = "https://lh4.googleusercontent.com/-5Y__F7y9tT8/AAAAAAAAAAI/AAAAAAAAAAA/APJypA0c8GceAD1En_08aa9B9Db6-nzfvg/s96-c/photo.jpg";
 
 }]);
